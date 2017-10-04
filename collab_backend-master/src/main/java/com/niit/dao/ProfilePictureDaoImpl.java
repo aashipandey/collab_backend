b518e098ddc781b5412c755faf4d599d270c7212
aashipandey <aashigungunpandey@gmail.com>
2017-10-04 16:22:54 +0530

    Add files via upload

---
+++ collab_backend-master/src/main/java/com/niit/dao/ProfilePictureDaoImpl.java
@@ -0,0 +1,51 @@
+package com.niit.dao;
+
+import java.util.List;
+
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import com.niit.model.ProfilePicture;
+
+@Repository
+@Transactional
+public class ProfilePictureDaoImpl implements ProfilePictureDao {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+
+	
+	public ProfilePicture getProfilePicture(String username) {
+		Session session=sessionFactory.openSession();
+		//select * from profilepicture where username='admin'
+		ProfilePicture profilePicture=(ProfilePicture)session.get(ProfilePicture.class, username);
+		session.flush();
+		session.close();
+		return profilePicture;
+	}
+
+
+
+	@Override
+	public void saveProfilePicture(ProfilePicture profilePicture) {
+		Session session=sessionFactory.openSession();
+		session.saveOrUpdate(profilePicture);
+		session.flush();
+		session.close();
+		
+	}
+
+	
+
+	
+	
+}
